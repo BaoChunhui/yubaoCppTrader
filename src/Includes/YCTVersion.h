18a1fd19ba5a077504b81dbfa4033fb4845d996c
BaoChunhui <32533415+BaoChunhui@users.noreply.github.com>
2026-03-10 15:16:09 +0800

    Add files via upload

---
+++ src/Includes/YCTVersion.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define YCT_VER_MAJOR 1
+#define YCT_VER_MINOR 0
+#define YCT_VER_PATCH 0
+
+static const char YCT_VERSION[] = "v1.0.0";
+static const char YCT_PRODUCT[] = "yubaoCppTrader_v1.0.0";
