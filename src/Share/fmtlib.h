da1429ca077ce3b088e1d09f342ff8ab59813c2a
BaoChunhui <32533415+BaoChunhui@users.noreply.github.com>
2026-03-12 16:03:55 +0800

    Add files via upload

--- src/Share/fmtlib.h
+++ src/Share/fmtlib.h
@@ -25,28 +25,24 @@ namespace fmtutil
 		return s;
 	}
 
-	// 带缓冲区长度的安全版本：保证在末尾写入 '\0'（超出长度则在末尾截断）
+	// 带缓冲区长度的安全版本：最多写 buf_size-1 个字符，并保证以 '\0' 结尾（超出则截断）
 	template<typename... Args>
 	inline char* format_to(char* buffer, std::size_t buf_size, const char* format, const Args& ...args)
 	{
 		if (buf_size == 0)
 			return buffer;
 
-		char* s = fmt::format_to(buffer, format, args...);
-		*s = '\0';
-		buffer[buf_size - 1] = '\0';
-
-		// 若 fmt 写超出了 buf_size，返回值也夹紧到 buffer 末尾，避免返回野指针
-		return (s < buffer + buf_size) ? s : (buffer + buf_size - 1);
+		auto result = fmt::format_to_n(buffer, buf_size - 1, format, args...);
+		*result.out = '\0';
+		return result.out;
 	}
 
 	template<int BUFSIZE=512, typename... Args>
 	inline const char* format(const char* format, const Args& ...args)
 	{
 		thread_local static char buffer[BUFSIZE];
-		char* s = fmt::format_to(buffer, format, args...);
-		*s = '\0';
-		buffer[BUFSIZE - 1] = '\0';
+		auto result = fmt::format_to_n(buffer, BUFSIZE - 1, format, args...);
+		*result.out = '\0';
 		return buffer;
 	}
 }
