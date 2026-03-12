840b757fc76f11bbbdd712e363eb6cdb577d4982
BaoChunhui <32533415+BaoChunhui@users.noreply.github.com>
2026-03-12 14:37:22 +0800

    Add files via upload

--- src/Share/fmtlib.h
+++ src/Share/fmtlib.h
@@ -3,7 +3,7 @@
  * \project	yubaoCppTrader
  *
  * \author yubao
- * \date 2026/03/11
+ * \date 2026/03/12
  * 
  * \brief fmtlib的封装，实现fmt::format和fmt::format_to的封装
  */
@@ -13,6 +13,7 @@
 #define FMT_HEADER_ONLY
 #endif
 #include <spdlog/fmt/bundled/format.h>
+#include <cstddef>
 
 namespace fmtutil
 {
@@ -22,6 +23,21 @@ namespace fmtutil
 		char* s = fmt::format_to(buffer, format, args...);
 		*s = '\0';
 		return s;
+	}
+
+	// 带缓冲区长度的安全版本：保证在末尾写入 '\0'（超出长度则在末尾截断）
+	template<typename... Args>
+	inline char* format_to(char* buffer, std::size_t buf_size, const char* format, const Args& ...args)
+	{
+		if (buf_size == 0)
+			return buffer;
+
+		char* s = fmt::format_to(buffer, format, args...);
+		*s = '\0';
+		buffer[buf_size - 1] = '\0';
+
+		// 若 fmt 写超出了 buf_size，返回值也夹紧到 buffer 末尾，避免返回野指针
+		return (s < buffer + buf_size) ? s : (buffer + buf_size - 1);
 	}
 
 	template<int BUFSIZE=512, typename... Args>
